8464be228e7958e932900554eb3a5f0271db9a0f
Akasibe <uchennaakasibe@gmail.com>
2022-10-18 18:11:35 -0700

    head

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add - main entry point
+* @num1: integer 1
+* @num2: integer 2
+* Description: Adds two numbers
+* Return: sum
+*/
+int add(int num1, int num2)
+{
+	int sum;
+
+
+	sum = num1 + num2;
+	return (sum);
+}
