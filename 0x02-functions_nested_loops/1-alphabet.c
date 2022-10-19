8621eea4f03ee1af5d03f82986ecc7166acd77f5
Akasibe <uchennaakasibe@gmail.com>
2022-10-18 17:16:43 -0700

    head

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - prints alphabet in lowercase
+*
+* Description: Write a function that prints the alphabet, in lowercase
+* Return: Always 0 (succes)
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _islower - main entry point
+*
+* Description: function that checks for lowercase character
+* @c: the integer value it receives
+* Return: 1 is true. 0 is false.
+*/
+int _islower(int c)
+{
+	int i = 'a';
+
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		/* refer into c */
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
