d36cd4c3cd60e28a59f88ef1da23833c8c009c0c
Akasibe <uchennaakasibe@gmail.com>
2022-10-18 18:49:45 -0700

    head

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - print sum of multiples
+* Return: Always (Success)
+*/
+int main(void)
+{
+	int n, z = 0;
+
+
+	while (n < 1024)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+			z += n;
+		}
+
+
+		n++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
